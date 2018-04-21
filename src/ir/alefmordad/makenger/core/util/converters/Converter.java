28c100f16951c66ad01c95c571c0ac9c8e058576
MAHDI <m.ranjbar3@rayana.ir>
2018-04-21 11:38:16 +0430

    change project to maven project.

--- src/ir/alefmordad/makenger/core/util/converters/Converter.java
+++
@@ -1,9 +0,0 @@
-package ir.alefmordad.makenger.core.util.converters;
-
-import java.sql.SQLException;
-
-public interface Converter<F, T> {
-
-    T convert(F from) throws SQLException;
-
-}
--- src/ir/alefmordad/makenger/core/util/converters/rowmapper/RowMapper.java
+++
@@ -1,8 +0,0 @@
-package ir.alefmordad.makenger.core.util.converters.rowmapper;
-
-import ir.alefmordad.makenger.core.util.converters.Converter;
-
-import java.sql.ResultSet;
-
-public interface RowMapper<T> extends Converter<ResultSet, T> {
-}
